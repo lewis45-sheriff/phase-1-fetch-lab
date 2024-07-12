634a32ce9b1181944cc46e91149f9cf86cfbf501
lewis <lewiskipkemoi53@gmail.com>
2024-07-12 18:53:37 +0300

    all

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Return the fetch call to ensure the promise is completed
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json()) // Convert the response to JSON
+    .then(data => renderBooks(data)); // Pass the data to renderBooks
 }
 
 function renderBooks(books) {
